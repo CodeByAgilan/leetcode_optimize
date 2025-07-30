580805814eb53ec123484f8ff1a5f32771cd65c2
Agilan <agilanmithu@gmail.com>
2025-07-30 10:39:11 +0530

    Time: 3 ms (82.57%), Space: 61.5 MB (8.3%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
@@ -1,5 +1,28 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        
+        int max=0,maxlen=0,tem=nums[0],maxlength=0;
+        for(int i=0;i<nums.length;i++){
+              if(nums[i]==tem&&nums[i]>max){
+                max=nums[i];
+                tem=nums[i];
+              }
+              if(nums[i]==max){
+                maxlen++;
+                if(maxlen>maxlength){
+                    maxlength=maxlen;
+                }
+              }
+              else if(nums[i]>max){
+                 max=nums[i];
+                 tem=nums[i];
+                 maxlen=1;
+                 maxlength=maxlen;
+              }
+              else{
+                  tem=nums[i];
+                  maxlen=0;
+              }
+        }
+        return maxlength;
     }
 }
