c395b5ffb7f651afd63255c2e1fa04df41fa229b
Agilan <agilanmithu@gmail.com>
2025-10-01 22:25:36 +0530

    Time: 0 ms (100%), Space: 40.5 MB (43.3%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+      int count=numBottles;
+      while(numBottles>=numExchange){
+        int rem=numBottles%numExchange;
+        numBottles/=numExchange;
+        count+=numBottles;
+        numBottles+=rem;
+      }
+      return count;
+    }
+}
